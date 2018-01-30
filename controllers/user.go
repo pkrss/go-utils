4fe5a6700860583f4c95ef7151b50fdbf419007e
liandeliang <liandeliang@gmail.com>
2018-01-30 17:36:56 +0800

    add sample code in main.go

--- controllers/user.go
+++ controllers/user.go
@@ -3,7 +3,7 @@ package controllers
 type ControllerUserInterface interface {
 	TokenKey() string
 	LoadTokenObj(token string) interface{}
-	SaveTokenObj(token string, obj interface{}) string
+	SaveTokenObj(token string, obj interface{})
 }
 
 var UserController ControllerUserInterface
