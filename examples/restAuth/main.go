59d1a52e0e819cfe72165e2adb1fe5b0ee74c459
liandeliang <liandeliang@gmail.com>
2018-02-05 18:00:07 +0800

    sample add comment

--- examples/restAuth/main.go
+++ examples/restAuth/main.go
@@ -16,17 +16,21 @@ import (
 
 func main() {
 
+	// create db connect
 	pqsql.Db = pqsql.CreatePgSql()
 	orm.DefaultOrmAdapter = &pqsql.PgSqlAdapter{}
 	pkControllers.DefaultAuthImpl = &auth.MyAuthImpl{}
 
+	// mvc sample 1: used parameter create rest service
 	pkRouters.AddRouter("/oauth/apps/{id:\\d+}", myControllers.CreateOAuthAppRestController())
 	pkRouters.AddRouterOptSlash("/oauth/apps", myControllers.CreateOAuthAppListRestController())
 
+	// mvc sample 2: used controller create rest service
 	pkRouters.AddRouter("/users/{id:\\.+}", &myControllers.UserIdController{})
 	pkRouters.AddRouterOptSlash("/users", &myControllers.UserController{})
 	pkRouters.AddRouterOptSlash("/users/login", &myControllers.UserController{}, "get:Login")
 
+	// start server
 	localAddr := "127.0.0.1:8080"
 	log.Printf("Server bind in %s\n", localAddr)
 	log.Fatal(http.ListenAndServe(localAddr, pkRouters.GetApp()))
