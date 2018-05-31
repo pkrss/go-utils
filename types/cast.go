bca2a510f655b38751586d7c63cc13a795edece3
liandeliang <liandeliang@gmail.com>
2018-05-31 17:39:42 +0800

    add cast

--- types/cast.go
+++ types/cast.go
@@ -25,9 +25,37 @@ func CastToInt64(o interface{}) (ret int64, err error) {
 		ret = int64(v)
 	case string:
 		ret, err = strconv.ParseInt(v, 10, 64)
-		return
 	default:
 		err = fmt.Errorf("Unknown type to int64 error: %v %T", o, o)
+	}
+	return
+}
+
+func CastToInt(o interface{}) (ret int, err error) {
+	if o == nil {
+		return
+	}
+	t := reflect.ValueOf(o)
+	if t.Kind() == reflect.Ptr {
+		return CastToInt(t.Elem().Interface())
+	}
+	var v2 int64
+	switch v := o.(type) {
+	case int64:
+		ret = int(v)
+	case int:
+		ret = v
+	case float32:
+		ret = int(v)
+	case float64:
+		ret = int(v)
+	case string:
+		v2, err = strconv.ParseInt(v, 10, 32)
+		if err == nil {
+			ret = int(v2)
+		}
+	default:
+		err = fmt.Errorf("Unknown type to int error: %v %T", o, o)
 	}
 	return
 }
