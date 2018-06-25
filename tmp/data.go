d0eeb586cc5a85b66afc6ffe6eef177b483f0e4e
liandeliang <liandeliang@gmail.com>
2018-06-25 11:16:11 +0800

    modify tmp data

--- tmp/data.go
+++ tmp/data.go
@@ -12,6 +12,9 @@ import (
 var dataMapLocker *sync.RWMutex = &sync.RWMutex{}
 var dataMap map[string]interface{} = make(map[string]interface{})
 
+var dataMapInvalidLocker *sync.RWMutex = &sync.RWMutex{}
+var dataMapInvalid map[string]int64 = make(map[string]int64)
+
 func DataSet(key string, obj interface{}) {
 	dataMapLocker.Lock()
 
@@ -30,9 +33,48 @@ func DataGet(key string) (obj interface{}, ok bool) {
 	return
 }
 
+func DataDelete(key string) {
+	dataMapLocker.Lock()
+
+	delete(dataMap, "tmpTime-"+key)
+	delete(dataMap, "tmp-"+key)
+	delete(dataMapInvalid, key)
+
+	dataMapLocker.Unlock()
+
+	return
+}
+
 func TmpDataSet(key string, obj interface{}) {
 	DataSet("tmpTime-"+key, strconv.FormatInt(time.Now().Unix(), 10))
 	DataSet("tmp-"+key, obj)
+}
+
+func TmpPeriodDataSet(key string, obj interface{}, invalidSeconds int64) {
+	DataSet("tmp-"+key, obj)
+
+	dataMapInvalidLocker.Lock()
+	dataMapInvalid[key] = time.Now().Unix() + invalidSeconds
+	dataMapInvalidLocker.Unlock()
+}
+
+func TmpRunOnce(key string, obj interface{}, invalidSeconds int64) {
+
+	dataMapInvalidLocker.Lock()
+	d := make([]string, 0)
+	now := time.Now().Unix()
+	for k, v := range dataMapInvalid {
+		if now > v {
+			d = append(d, k)
+		}
+	}
+
+	for _, v := range d {
+		delete(dataMapInvalid, v)
+		delete(dataMap, "tmp-"+v)
+	}
+
+	dataMapInvalidLocker.Unlock()
 }
 
 func TmpDataGet(key string, invalidSeconds int64) (obj interface{}, ok bool) {
@@ -44,6 +86,7 @@ func TmpDataGet(key string, invalidSeconds int64) (obj interface{}, ok bool) {
 	n, _ := strconv.ParseInt(i.(string), 10, 64)
 	now := time.Now().Unix()
 	if now-n > invalidSeconds {
+		DataDelete(key)
 		return nil, false
 	}
 	return DataGet("tmp-" + key)
