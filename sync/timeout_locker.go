a42c2ac9427b7532acf76026bf636b7e2b1d6292
liandeliang <liandeliang@gmail.com>
2019-07-20 22:58:45 +0800

    fix locker

--- sync/timeout_locker.go
+++ sync/timeout_locker.go
@@ -1,6 +1,7 @@
 package sync
 
 import (
+	s "sync"
 	"time"
 )
 
@@ -8,24 +9,33 @@ import (
 type TimeOutLocker struct {
 	done   chan int
 	locked TAtomBool
+	locker s.Mutex
 }
 
 // WaitTimeOut ...
 func (l *TimeOutLocker) WaitTimeOut(timeout time.Duration) {
-	l.done = make(chan int)
+
+	if l.done == nil {
+		l.done = make(chan int)
+	}
 	l.locked.Set(true)
 
 	select {
 	case <-time.After(timeout): // timed out
 	case <-l.done: // Wait returned
 	}
+
+	// l.locker.Lock()
 	l.locked.Set(false)
-	close(l.done)
+	// l.locker.Unlock()
 }
 
 // Unlock ...
 func (l *TimeOutLocker) Unlock() {
+	l.locker.Lock()
+	defer l.locker.Unlock()
 	if l.locked.Get() { // called directly or via defer, make sure we don't unlock if we don't have the lock
+		l.locked.Set(false)
 		l.done <- 1
 	}
 }
