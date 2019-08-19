b868ee9d9c92f43baeaffef76ec14cdd7312d42c
liandeliang <liandeliang@gmail.com>
2019-08-19 17:20:00 +0800

    add c.SleepMilliSeconds

--- sync/timeout_wait.go
+++ sync/timeout_wait.go
@@ -7,11 +7,15 @@ import (
 // WaitForTimeOut ...
 type WaitForTimeOut struct {
 	ref int32
+	SleepMilliSeconds int64
 }
 
 // WaitTimeOut ...
 func (l *WaitForTimeOut) WaitTimeOut(timeout time.Duration) {
-	s := 50 * time.Millisecond
+	if c.SleepMilliSeconds == 0 {
+		c.SleepMilliSeconds = 50
+	}
+	s := c.SleepMilliSeconds * time.Millisecond
 	for l.ref = int32(timeout / s); l.ref > 0; l.ref-- {
 		time.Sleep(s)
 	}
