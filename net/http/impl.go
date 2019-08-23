10ecca7cc5d8bab0e89bdf800f4015ad228fda7e
liandeliang <liandeliang@gmail.com>
2019-08-23 14:31:40 +0800

    optimize http client

--- net/http/impl.go
+++ net/http/impl.go
@@ -2,59 +2,294 @@ package impl
 
 import (
 	"bytes"
-	"os"
-
-	pkReflect "github.com/pkrss/go-utils/reflect"
+	"compress/gzip"
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
 
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"net/http/cookiejar"
+	"net/url"
 	"strings"
 	"time"
-
-	hc "github.com/ddliu/go-httpclient"
 )
 
-var OsExitWhenCloudflare50x = false
+// Method ...
+type Method int
 
 const (
-	Get      = iota //0
-	Post            //1
-	Put             //2
-	Patch           //3
-	Delete          //4
-	PutEmpty        //5
-	PostJSON        //6
+	// MethodUnknown ...
+	MethodUnknown Method = iota
+	Get
+	Post
+	Put
+	Patch
+	Delete
+	PutEmpty
+	PostJSON
 )
 
+func method2String(method Method) string {
+	ret := ""
+	switch method {
+	case Get:
+		ret = "GET"
+	case Post, PostJSON:
+		ret = "POST"
+	case Put, PutEmpty:
+		ret = "PUT"
+	case Patch:
+		ret = "PATCH"
+	case Delete:
+		ret = "DELETE"
+	}
+	return ret
+}
+func addUrlParams(url_ string, params url.Values) string {
+	if len(params) == 0 {
+		return url_
+	}
+
+	v := params.Encode()
+	if v != "" {
+		if strings.Contains(url_, "?") {
+			url_ += "&"
+		} else {
+			url_ += "?"
+		}
+		url_ += v
+	}
+
+	return url_
+}
+func toUrlValues(v interface{}) (ret url.Values, e error) {
+	switch t := v.(type) {
+	case url.Values:
+		ret = t
+	case map[string][]string:
+		ret = url.Values(t)
+	case map[string]string:
+		ret = make(url.Values)
+		for k, v := range t {
+			ret.Add(k, v)
+		}
+	case nil:
+		ret = make(url.Values)
+	default:
+		e = errors.New("Invalid toUrlValues value")
+	}
+	return
+}
+
+// Read response body into a byte slice.
+func readAll(res *http.Response) ([]byte, error) {
+	var reader io.ReadCloser
+	var err error
+	switch res.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, err = gzip.NewReader(res.Body)
+		if err != nil {
+			return nil, err
+		}
+		defer reader.Close()
+		return ioutil.ReadAll(reader)
+	default:
+		return ioutil.ReadAll(res.Body)
+	}
+
+}
+
 type HttpClient struct {
-	hc *hc.HttpClient
+	header    map[string]string
+	hc        *http.Client
+	transport *http.Transport
 }
 
 func (h *HttpClient) Init(proxy string) (e error) {
-	h.hc = hc.NewHttpClient()
+
+	h.header = make(map[string]string)
+	m := h.header
+
+	transport := &http.Transport{
+		// Proxy: ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   3 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	h.hc = &http.Client{Transport: transport}
+
+	// transport.Dial = func(network, addr string) (net.Conn, error) {
+	// 	var conn net.Conn
+	// 	var err error
+	// 	if connectTimeoutMS > 0 {
+	// 		conn, err = net.DialTimeout(network, addr, time.Duration(connectTimeoutMS)*time.Millisecond)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 	} else {
+	// 		conn, err = net.Dial(network, addr)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 	}
+
+	// 	if timeoutMS > 0 {
+	// 		conn.SetDeadline(time.Now().Add(time.Duration(timeoutMS) * time.Millisecond))
+	// 	}
+
+	// 	return conn, nil
+	// }
 
 	if proxy != "" {
-		m := make(map[interface{}]interface{})
-		m[hc.OPT_PROXY] = proxy
+		if !strings.Contains(proxy, "://") {
+			proxy = "http://" + proxy
+		}
 
-		jar, err := cookiejar.New(nil)
-		if err == nil {
-			m[hc.OPT_COOKIEJAR] = jar
+		if proxyUrl, err := url.Parse(proxy); err == nil {
+			transport.Proxy = http.ProxyURL(proxyUrl)
+		} else {
+			e = err
+			return
 		}
 
 		m["Pragma"] = "no-cache"
 		m["Cache-Control"] = "no-cache, must-revalidate"
-		h.hc.Defaults(m)
+
+		m["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+		m["Accept-Encoding"] = "gzip"
 	}
 
 	return nil
 }
 
-func (h *HttpClient) GetHttpClient() *hc.HttpClient {
+func (c *HttpClient) SendRequest(httpUrl string, params interface{}, method Method, header map[string]string) (res *http.Response, e error) {
+
+	h := make(map[string]string)
+	for k, v := range c.header {
+		if _, ok := h[k]; !ok {
+			h[k] = v
+		}
+	}
+
+	h["Content-Type"] = "application/x-www-form-urlencoded"
+
+	var body io.Reader
+	switch method {
+	case Post:
+		if params != nil {
+			if paramsValues, e2 := toUrlValues(params); e2 == nil {
+				body = strings.NewReader(paramsValues.Encode())
+			} else {
+				e = e2
+				return
+			}
+		}
+	case PostJSON:
+		h["Content-Type"] = "application/json"
+		if params != nil {
+			var content []byte
+			switch t := params.(type) {
+			case []byte:
+				content = t
+			case string:
+				content = []byte(t)
+			default:
+				if content, e = json.Marshal(params); e != nil {
+					return
+				}
+			}
+			body = bytes.NewBuffer(content)
+		}
+	default:
+		if paramsValues, e2 := toUrlValues(params); e2 == nil {
+			httpUrl = addUrlParams(httpUrl, paramsValues)
+		} else {
+			e = e2
+			return
+		}
+	}
+
+	if header != nil {
+		for k, v := range header {
+			h[k] = v
+		}
+	}
+
+	req, err := http.NewRequest(method2String(method), httpUrl, body)
+	if err != nil {
+		e = err
+		return
+	}
+
+	for k, v := range h {
+		req.Header.Set(k, v)
+	}
+
+	res, e2 := c.hc.Do(req)
+	if e2 != nil {
+		e = e2
+		return
+	}
+
+	if res.StatusCode >= 400 {
+		e = fmt.Errorf("http error statusCode=%v", res.StatusCode)
+
+		res.Body.Close()
+	}
+
+	return
+}
+
+func (c *HttpClient) FetchResult(httpUrl string, params interface{}, method Method, header map[string]string) (ret []byte, statusCode int, rspHeader http.Header, e error) {
+
+	res, e2 := c.SendRequest(httpUrl, params, method, header)
+
+	if res != nil {
+		rspHeader = res.Header
+		statusCode = res.StatusCode
+	}
+
+	if e2 != nil {
+		e = e2
+		return
+	}
+
+	defer res.Body.Close()
+
+	if ret, e = readAll(res); e != nil {
+		return
+	}
+
+	return
+}
+
+// func (h *HttpClient) onDoCloudflareError(statsCode int, rspHeader http.Header) {
+// 	if (statsCode >= 400) && (statsCode < 600) { // too busy  //
+// 		if rspHeader == nil {
+// 			return
+// 		}
+// 		v, ok := rspHeader["Server"]
+// 		if ok && (len(v) > 0) && (strings.Contains(v[0], "cloudflare")) {
+// 			time.Sleep(1 * time.Second)
+// 			if OsExitWhenCloudflare50x {
+// 				os.Exit(-97)
+// 			}
+// 			pkReflect.SetStructFieldValue(h.hc, "transport", nil, false)
+// 		}
+// 	}
+// }
+
+func (h *HttpClient) GetHttpClient() *http.Client {
 	return h.hc
 }
 
@@ -96,86 +331,39 @@ func (h *HttpClient) DoDeleteRetJson(httpUrl string, params map[string]string, r
 	return h.DoRequestRetJson(httpUrl, params, Delete, rsp, header)
 }
 
-func (h *HttpClient) DoRequest(httpUrl string, params interface{}, httpMethod int, header map[string]string) (ret []byte, e error) {
+func (h *HttpClient) DoRequest(httpUrl string, params interface{}, httpMethod Method, header map[string]string) (ret []byte, e error) {
 	ret, _, e = h.DoRequest2(httpUrl, params, httpMethod, header)
 	return
 }
 
-func (h *HttpClient) DoRequest2(httpUrl string, params interface{}, httpMethod int, header map[string]string) (ret []byte, statsCode int, e error) {
+func (h *HttpClient) DoRequest2(httpUrl string, params interface{}, httpMethod Method, header map[string]string) (ret []byte, statsCode int, e error) {
 	ret, statsCode, _, e = h.DoRequest2WithRetHeader(httpUrl, params, httpMethod, header)
 	return
 }
-func (h *HttpClient) DoRequest2WithRetHeader(httpUrl string, params interface{}, httpMethod int, header map[string]string) (ret []byte, statsCode int, rspHeader http.Header, e error) {
+func (h *HttpClient) DoRequest2WithRetHeader(httpUrl string, params interface{}, httpMethod Method, header map[string]string) (ret []byte, statsCode int, rspHeader http.Header, e error) {
 
-	var res *hc.Response
-
-	if res, e = h.DoRequest2WithRetRsp(httpUrl, params, httpMethod, header); e != nil {
-		return
-	}
-
-	defer res.Body.Close()
-
-	rspHeader = res.Header
-	statsCode = res.StatusCode
-
-	switch statsCode {
-	case 200, 301, 302:
-		ret, e = res.ReadAll()
-	default:
-		ret, _ = res.ReadAll()
-		e = fmt.Errorf("http error statusCode=%v", res.StatusCode)
-	}
-
-	h.onDoCloudflareError(statsCode, rspHeader)
-
-	return
+	return h.FetchResult(httpUrl, params, httpMethod, header)
 }
 
-func (h *HttpClient) DoRequest2WithRetRsp(httpUrl string, params interface{}, httpMethod int, header map[string]string) (res *hc.Response, e error) {
-
-	if params == nil {
-		params = make(map[string]string)
-	}
-
-	if header != nil && len(header) > 0 {
-		h.hc.WithHeaders(header)
-	}
-
-	switch httpMethod {
-	case Get:
-		res, e = h.hc.Get(httpUrl, params)
-	case Post:
-		res, e = h.hc.Post(httpUrl, params)
-	case PostJSON:
-		res, e = h.hc.PostJson(httpUrl, params)
-	case Put:
-		res, e = h.hc.PutJson(httpUrl, &params)
-	case PutEmpty:
-		res, e = h.hc.Put(httpUrl, nil)
-	case Patch:
-		res, e = h.hc.Patch(httpUrl, params.(map[string]string))
-	case Delete:
-		res, e = h.hc.Delete(httpUrl, params)
-	}
-
-	return
+func (h *HttpClient) DoRequest2WithRetRsp(httpUrl string, params interface{}, httpMethod Method, header map[string]string) (res *http.Response, e error) {
+	return h.SendRequest(httpUrl, params, httpMethod, header)
 }
 
-func (h *HttpClient) onDoCloudflareError(statsCode int, rspHeader http.Header) {
-	if (statsCode >= 400) && (statsCode < 600) { // too busy  //
-		if rspHeader == nil {
-			return
-		}
-		v, ok := rspHeader["Server"]
-		if ok && (len(v) > 0) && (strings.Contains(v[0], "cloudflare")) {
-			time.Sleep(1 * time.Second)
-			if OsExitWhenCloudflare50x {
-				os.Exit(-97)
-			}
-			pkReflect.SetStructFieldValue(h.hc, "transport", nil, false)
-		}
-	}
-}
+// func (h *HttpClient) onDoCloudflareError(statsCode int, rspHeader http.Header) {
+// 	if (statsCode >= 400) && (statsCode < 600) { // too busy  //
+// 		if rspHeader == nil {
+// 			return
+// 		}
+// 		v, ok := rspHeader["Server"]
+// 		if ok && (len(v) > 0) && (strings.Contains(v[0], "cloudflare")) {
+// 			time.Sleep(1 * time.Second)
+// 			if OsExitWhenCloudflare50x {
+// 				os.Exit(-97)
+// 			}
+// 			pkReflect.SetStructFieldValue(h.hc, "transport", nil, false)
+// 		}
+// 	}
+// }
 
 func (h *HttpClient) DoRequestPostJson(httpUrl string, jsonData interface{}, header map[string]string) (ret []byte, statsCode int, e error) {
 	ret, statsCode, _, e = h.DoRequestPostJsonWithRetHeader(httpUrl, jsonData, header)
@@ -186,78 +374,22 @@ func (h *HttpClient) DoRequestPostJsonWithRetHeader(httpUrl string, jsonData int
 	return h.DoRequestJsonWithRetHeader2(httpUrl, jsonData, header, Post)
 }
 
-func (h *HttpClient) DoRequestJsonWithRetHeader2(httpUrl string, jsonData interface{}, header map[string]string, method int) (ret []byte, statsCode int, rspHeader http.Header, e error) {
-	if jsonData == nil {
-		jsonData = make(map[string]string)
-	}
-
-	var res *hc.Response
-
-	sMethod := "GET"
-	if method == Post {
-		sMethod = "POST"
-	} else if method == Put {
-		sMethod = "PUT"
-	} else if method == Delete {
-		sMethod = "DELETE"
-	} else if method == Patch {
-		sMethod = "PATCH"
-	}
-	// else if method == Option {
-	// 	sMethod = "OPTION"
-	// }
-
-	res, e = h.SendJson(sMethod, httpUrl, jsonData, header)
-
-	if e != nil {
-		return
-	}
-
-	defer res.Body.Close()
-
-	rspHeader = res.Header
-
-	statsCode = res.StatusCode
-
-	switch statsCode {
-	case 200, 301, 302:
-		ret, e = res.ReadAll()
-	default:
-		ret, _ = res.ReadAll()
-		e = fmt.Errorf("http error statusCode=%v", res.StatusCode)
-	}
-
-	h.onDoCloudflareError(statsCode, rspHeader)
-
-	return
+func (h *HttpClient) DoRequestJsonWithRetHeader2(httpUrl string, jsonData interface{}, header map[string]string, method Method) (ret []byte, statsCode int, rspHeader http.Header, e error) {
+	return h.FetchResult(httpUrl, jsonData, method, header)
 }
 
-func (h *HttpClient) SendJson(method string, url string, data interface{}, headers map[string]string) (*hc.Response, error) {
+func (h *HttpClient) SendJson(method Method, url string, data interface{}, headers map[string]string) (*http.Response, error) {
 	if headers == nil {
 		headers = make(map[string]string)
 	}
 	headers["Content-Type"] = "application/json"
-
-	var body []byte
-	switch t := data.(type) {
-	case []byte:
-		body = t
-	case string:
-		body = []byte(t)
-	default:
-		var err error
-		body, err = json.Marshal(data)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return h.hc.Do(method, url, headers, bytes.NewReader(body))
+	return h.SendRequest(url, data, method, headers)
 }
 
-func (h *HttpClient) DoRequestRetJson(httpUrl string, params map[string]string, httpMethod int, ret interface{}, header map[string]string) (e error) {
+func (h *HttpClient) DoRequestRetJson(httpUrl string, params map[string]string, httpMethod Method, ret interface{}, header map[string]string) (e error) {
 	var content []byte
-	content, e = h.DoRequest(httpUrl, params, httpMethod, header)
+
+	content, _, _, e = h.FetchResult(httpUrl, params, httpMethod, header)
 	if e != nil {
 		log.Println(string(content))
 		return
